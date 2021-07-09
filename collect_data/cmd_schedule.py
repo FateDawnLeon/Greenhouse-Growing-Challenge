f281324787cbe1354fe3f7d199c526a0f4359bbf
Xi Chen <xxichen@ucdavis.edu>
2021-07-09 08:53:11 +0800

    update bo search on sim B

--- collect_data/cmd_schedule.py
+++ collect_data/cmd_schedule.py
@@ -4,55 +4,45 @@ import pytz
 from datetime import date, datetime
 
 
-def run_simB_BO():
-    simulator = 'B'
-    num_calls = 1000
-    num_init_points = 100
-    dimension_spec = 'BB'
-    optimizer = 'gbrt'
-    precision = 1
+def run_BO(simulator='A', num_calls=100, num_init_points=10, dimension_spec='A1', optimizer='gp', precision=0, plan=False):
     today = date.today().isoformat()
 
     cmd = f'python bo_search.py \
+            -L \
             -NC {num_calls} \
             -NI {num_init_points} \
             -DS {dimension_spec} \
             -S {simulator} \
             -O {optimizer} \
-            -L \
             -P {precision} \
             -D data_sample=BO_date={today}_SIM={simulator}_DS={dimension_spec}_OPT={optimizer}_NI={num_init_points}_NC={num_calls}_P={precision}'
 
-    timezone = pytz.timezone('Europe/Amsterdam')
-    start_time = datetime(2021, 7, 5, hour=14, tzinfo=timezone)
-    while datetime.now(tz=timezone) < start_time:
-        print('waiting to start... current time >>>' ,datetime.now(tz=timezone), end='\r')
-        time.sleep(1)
-
-    os.system(cmd)
-
-
-def run_simA_BO():
-    simulator = 'A'
-    num_calls = 5000
-    num_init_points = 500
-    dimension_spec = 'AA'
-    optimizer = 'gbrt'
-    precision = 0
-    today = date.today().isoformat()
-
-    cmd = f'python bo_search.py \
-            -S {simulator} \
-            -DS {dimension_spec} \
-            -NI {num_init_points} \
-            -NC {num_calls} \
-            -P {precision} \
-            -L \
-            -D bo_data_{today}_SIM={simulator}__DS={dimension_spec}_OPT={optimizer}_NI={num_init_points}_NC={num_calls}_P={precision}'
+    if plan:
+        timezone = pytz.timezone('Europe/Amsterdam')
+        start_time = datetime(2021, 7, 8, hour=14, tzinfo=timezone)
+        while datetime.now(tz=timezone) < start_time:
+            print('waiting to start... current time >>>' ,datetime.now(tz=timezone), end='\r')
+            time.sleep(1)
 
     os.system(cmd)
 
 
 if __name__ == '__main__':
-    run_simB_BO()
-    # run_simA_BO()
+    # run_BO(
+    #     simulator='B',
+    #     num_calls=300,
+    #     num_init_points=30,
+    #     dimension_spec='B3',
+    #     optimizer='gbrt',
+    #     precision=2,
+    #     plan=False
+    # )
+    run_BO(
+        simulator='B',
+        num_calls=100,
+        num_init_points=10,
+        dimension_spec='B4',
+        optimizer='gbrt',
+        precision=1,
+        plan=False
+    )
