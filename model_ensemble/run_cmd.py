c87d811c48ba2209ecc27b289fbb4c2c93025c18
Xi Chen <xxichen@ucdavis.edu>
2021-07-11 10:22:28 +0800

    [minor] update model training script

--- model_ensemble/run_cmd.py
+++ model_ensemble/run_cmd.py
@@ -4,35 +4,38 @@ import string
 
 
 wd = 0
-reparse_all_data = False
 
 
 def id_generator(N=8):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
 
 
-def train_single_model(train_dirs, val_dirs):
+def train_single_model(train_dirs, val_dirs, reparse_data=False):
     cmd = f'python train.py --max-iters 200000 --batch-size 256 --lr 0.001 --wd {wd} \
         --root-dir trained_models/single_model \
         --train-dirs {train_dirs} \
         --val-dirs {val_dirs}'
-    if reparse_all_data:
+    if reparse_data:
         cmd += ' -FP'
     os.system(cmd)
 
 
-def train_ensemble_model(train_dirs, val_dirs, N=10):
+def train_ensemble_model(train_dirs, val_dirs, N=10, reparse_data=False):
     model_id = id_generator()
     for n in range(N):
         cmd = f'python train.py --max-iters 10000 --batch-size 256 --lr 0.001 --wd {wd} \
             --root-dir trained_models/ensemble_model_{model_id}_child[{n}] \
             --train-dirs {train_dirs} \
             --val-dirs {val_dirs}'
-        if reparse_all_data and n == 0:
+        if reparse_data and n == 0:
             cmd += ' -FP'
         os.system(cmd)
 
 
 if __name__ == '__main__':
-    train_single_model()
-    # train_ensemble_model()
+    reparse_data = True
+    train_dirs = "TRAIN_DATA_DIR1 TRAIN_DATA_DIR2 ..."  # TODO: change data dirs to your owns
+    val_dirs = "VAL_DATA_DIR1 VAL_DATA_DIR2 ..."  # TODO: change data dirs to your owns
+
+    # train_single_model(train_dirs, val_dirs, reparse_data=reparse_data)
+    # train_ensemble_model(train_dirs, val_dirs, reparse_data=reparse_data)
