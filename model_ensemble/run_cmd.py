2f01130cdafec857ae3784c212558865f98e6c80
Xi Chen <xxichen@ucdavis.edu>
2021-07-11 10:16:57 +0800

    [minor] preprocessing data only once for one training run

--- model_ensemble/run_cmd.py
+++ model_ensemble/run_cmd.py
@@ -11,24 +11,24 @@ def id_generator(N=8):
     return ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
 
 
-def train_single_model():
+def train_single_model(train_dirs, val_dirs):
     cmd = f'python train.py --max-iters 200000 --batch-size 256 --lr 0.001 --wd {wd} \
         --root-dir trained_models/single_model \
-        --train-dirs YOUR_OWN_DIRS \
-        --val-dirs YOUR_OWN_DIRS'
+        --train-dirs {train_dirs} \
+        --val-dirs {val_dirs}'
     if reparse_all_data:
         cmd += ' -FP'
     os.system(cmd)
 
 
-def train_ensemble_model(N=10):
+def train_ensemble_model(train_dirs, val_dirs, N=10):
     model_id = id_generator()
     for n in range(N):
         cmd = f'python train.py --max-iters 10000 --batch-size 256 --lr 0.001 --wd {wd} \
             --root-dir trained_models/ensemble_model_{model_id}_child[{n}] \
-            --train-dirs YOUR_OWN_DIRS \
-            --val-dirs YOUR_OWN_DIRS'
-        if reparse_all_data:
+            --train-dirs {train_dirs} \
+            --val-dirs {val_dirs}'
+        if reparse_all_data and n == 0:
             cmd += ' -FP'
         os.system(cmd)
 
